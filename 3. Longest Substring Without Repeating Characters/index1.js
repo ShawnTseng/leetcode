33b704dcb7f3429712b75ab2bc1e188a73313cd8
Shawn Tseng <shawntseng40@gmail.com>
2022-07-23 09:16:30 +0800

    change file name

--- 3. Longest Substring Without Repeating Characters/index1.js
+++ 3. Longest Substring Without Repeating Characters/index1.js
@@ -1,15 +1,15 @@
 // Brute Force
 const lengthOfLongestSubstring = function (s) {
     const n = s.length;
-    let res = 0;
+    let result = 0;
     for (let i = 0; i < n; i++) {
         for (let j = i; j < n; j++) {
             if (checkRepetition(s, i, j)) {
-                res = Math.max(res, j - i + 1);
+                result = Math.max(result, j - i + 1);
             }
         }
     }
-    return res;
+    return result;
 }
 
 const checkRepetition = function(s, start, end) {
